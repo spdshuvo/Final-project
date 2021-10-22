31f2e36683ede11805601514cdee68814d7ea907
spdshuvo <shuvodev47@.gmail>
2021-10-23 01:42:54 +0600

    html enter

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,10 @@
 $(function(){
     'use strict';
+    $(".banner_container").slick({
+        arrows:false,
+    });
+    // testimonial slick activition
+    $(".testimonial_slider_area").slick({
+        arrows:false,
+    });
 })
